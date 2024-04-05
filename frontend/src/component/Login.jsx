8741ce8485cbc521b9420b7aa4992a23f5096249
Prachi <prachik3002@gmail.com>
2024-04-06 00:04:43 +0530

    Update Login component with new styling and move to App.css
    * Updated Login component to have new styling with a black background and centered text for the title.
    * Moved the CSS for the Login component to App.css.
    * Removed unused imports and unnecessary div elements for simplification.

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -1,44 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 const Login = () => {
   return (
-    <div className="container mt-5">
-        <form className='container d-flex-column bg-primary'>  
-            <div>
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                Email address
-                </label>
-                <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">
-                Password
-                </label>
-                <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-                />
-            </div>
-            <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">
-                Check me out
-                </label>
-            </div>
-            <button type="submit" className="btn btn-primary">
-                Submit
-            </button>
-        </form>
+    <div className="myCard container mt-5 d-flex-column text-light">
+      <h1 className="pt-3 text-light text-center">Login</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+       
+        <button type="submit" className="btn btn-primary mb-5">
+          Submit
+        </button>
+      </form>
     </div>
-        
-
   )
 }
 
