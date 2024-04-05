389f60bf765a24fe902d1e38b590ba8f4cf85912
Prachi <prachik3002@gmail.com>
2024-04-05 22:52:05 +0530

    Login&Signup

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>
+        <Link to='/Login'>Login</Link><br />
+        <Link to='/Signup'>Signup</Link>
+      </h1>
     </div>
   )
 }
